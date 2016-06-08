9cac0f2f17375b39999d6d8f0746e854dce93b0e
sofili <slee8@L-SNVH0LCG8W-M.local>
2016-06-08 16:33:52 -0700

    added heroku support

--- index.js
+++ index.js
@@ -3,3 +3,27 @@ module.exports = {
   logLevels: require('./lib/logger.js').logLevels,
   Wit: require('./lib/wit.js').Wit,
 }
+
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.listen((process.env.PORT || 3000));
+
+// Server frontpage
+app.get('/', function (req, res) {
+    res.send('This is TestBot Server');
+});
+
+// Facebook Webhook
+app.get('/webhook', function (req, res) {
+    if (req.query['hub.verify_token'] === 'testbot_verify_token') {
+        res.send(req.query['hub.challenge']);
+    } else {
+        res.send('Invalid verify token');
+    }
+});
