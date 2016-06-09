310ca092b05b4a9e3b299af6ca303505bad23394
sofili <slee8@L-SNVH0LCG8W-M.local>
2016-06-08 17:51:30 -0700

    test

--- index.js
+++ index.js
@@ -60,22 +60,22 @@ const fbMessage = (recipientId, msg, cb) => {
   });
 };
 
-// // See the Webhook reference
-// // https://developers.facebook.com/docs/messenger-platform/webhook-reference
-// const getFirstMessagingEntry = (body) => {
-//   const val = body.object == 'page' &&
-//     body.entry &&
-//     Array.isArray(body.entry) &&
-//     body.entry.length > 0 &&
-//     body.entry[0] &&
-//     body.entry[0].id === FB_PAGE_ID &&
-//     body.entry[0].messaging &&
-//     Array.isArray(body.entry[0].messaging) &&
-//     body.entry[0].messaging.length > 0 &&
-//     body.entry[0].messaging[0]
-//   ;
-//   return val || null;
-// };
+// See the Webhook reference
+// https://developers.facebook.com/docs/messenger-platform/webhook-reference
+const getFirstMessagingEntry = (body) => {
+  const val = body.object == 'page' &&
+    body.entry &&
+    Array.isArray(body.entry) &&
+    body.entry.length > 0 &&
+    body.entry[0] &&
+    body.entry[0].id === FB_PAGE_ID &&
+    body.entry[0].messaging &&
+    Array.isArray(body.entry[0].messaging) &&
+    body.entry[0].messaging.length > 0 &&
+    body.entry[0].messaging[0]
+  ;
+  return val || null;
+};
 
 // // Wit.ai bot specific code
 
