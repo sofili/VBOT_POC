8f27fec4e17680b7f5cb8735a058e7f6b022b4bd
sofili <slee8@L-SNVH0LCG8W-M.local>
2016-06-08 17:49:10 -0700

    test

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ module.exports = {
 
 // Webserver parameter
 const PORT = process.env.PORT || 8445;
-const Wit = require('node-wit').Wit
+const Wit = require('./lib/wit.js').Wit
 // Wit.ai parameters
 const WIT_TOKEN = '2T7FBUGWU3EZMQI5LR6TOZ7XJT3PP47W';
 
