8ec3e554fecb781b0af35d9ffe838c3b35e283b2
sofili <slee8@L-SNVH0LCG8W-M.local>
2016-06-10 15:54:15 -0700

    test

--- index.js
+++ index.js
@@ -295,7 +295,7 @@ app.post('/webhook', (req, res) => {
             // }
 
             // Updating the user's current session state
-            console.log('sending to fb:' + stringify(context));
+            console.log('sending to fb:' + JSON.stringify(context));
             sessions[sessionId].context = context;
           }
         }
