3266d823ec7d212728281aeb8694f28c494af6d4
sofili <slee8@L-SNVH0LCG8W-M.local>
2016-06-15 11:09:34 -0700

    fix bug

--- index.js
+++ index.js
@@ -49,37 +49,35 @@ const fbMessage = (recipientId, msg, cb) => {
         id: recipientId,
       },
       message: {
-        {
-    "attachment": {
-      "type": "template",
-      "payload": {
-        "template_type": "generic",
-        "elements": [{
-          "title": "First card",
-          "subtitle": "Element #1 of an hscroll",
-          "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-          "buttons": [{
-            "type": "web_url",
-            "url": "https://www.messenger.com/",
-            "title": "Web url"
-          }, {
-            "type": "postback",
-            "title": "Postback",
-            "payload": "Payload for first element in a generic bubble",
-          }],
-        },{
-          "title": "Second card",
-          "subtitle": "Element #2 of an hscroll",
-          "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-          "buttons": [{
-            "type": "postback",
-            "title": "Postback",
-            "payload": "Payload for second element in a generic bubble",
-          }],
-        }]
-      }
-    }
-  }//text: msg,
+	    attachment: {
+	      type: template,
+	      payload: {
+	        "template_type": "generic",
+	        "elements": [{
+	          "title": "First card",
+	          "subtitle": "Element #1 of an hscroll",
+	          "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+	          "buttons": [{
+	            "type": "web_url",
+	            "url": "https://www.messenger.com/",
+	            "title": "Web url"
+	          }, {
+	            "type": "postback",
+	            "title": "Postback",
+	            "payload": "Payload for first element in a generic bubble",
+	          }],
+	        },{
+	          "title": "Second card",
+	          "subtitle": "Element #2 of an hscroll",
+	          "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+	          "buttons": [{
+	            "type": "postback",
+	            "title": "Postback",
+	            "payload": "Payload for second element in a generic bubble",
+	          }],
+	        }]
+	      }
+	    }
       },
     },
   };
