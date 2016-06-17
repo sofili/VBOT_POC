7f0d70047eaf25f8b01bbcef4133a8fdf37b5843
sofili <slee8@L-SNVH0LCG8W-M.local>
2016-06-16 23:55:09 -0700

    test

--- index.js
+++ index.js
@@ -823,7 +823,6 @@ function getSimilarMovie(text, cb) {
 				// 	.catch(function (err) {
 				// 		console.log('111*******Error sending message: ', err);
 				// });
-				setTimeout(return "!!!!test", 4000);
 		})
 		.then(function(similarMoviesArray) {
 			console.log("final then!");
