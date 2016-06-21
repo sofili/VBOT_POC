93b40abf439a2851459af49c1089e37a3a8dfbec
sofili <slee8@L-SNVH0LCG8W-M.local>
2016-06-21 16:19:58 -0700

    tets

--- index.js
+++ index.js
@@ -739,7 +739,7 @@ function getMovieDetail(vuduContent) {
 					console.log('cannot find a review for contentId:' + contentId);
 				}
 				else {
-				  	vuduContent.description = evaluation.content[0].description[0];
+				  	vuduContent.description = evaluation.content[0].description[0] ?? "";
 				  	vuduContent.releaseTime = evaluation.content[0].releaseTime[0];
 				  	vuduContent.mpaaRating = evaluation.content[0].mpaaRating[0];
 				  	vuduContent.tomatoMeter = evaluation.content[0].tomatoMeter[0];
